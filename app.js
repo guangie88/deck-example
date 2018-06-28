d84701f36ffcf729967be85943c3a9690fc291eb
Weiguang <chen.weiguang@gmail.com>
2018-06-28 09:04:22 +0800

    Working set-up (#1)
    * Add server.js
    
    * Basic node set-up
    
    * Update package

---
+++ app.js
@@ -0,0 +1,47 @@
+import { Deck, MapController } from "@deck.gl/core";
+import { GeoJsonLayer } from "@deck.gl/layers";
+import Map from "./mapbox";
+
+// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
+const GEOJSON =
+  "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_1_states_provinces_shp.geojson"; // eslint-disable-line
+// Set your mapbox token here
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoiZ3VhbmdpZSIsImEiOiJjaml4N2gxOGowMDN5M3FwaHJqOGZobGdkIn0.IXqf-xh73aRmPpNRVOzBRg"; // eslint-disable-line
+
+const INITIAL_VIEW_STATE = {
+  latitude: 1.351267595658125,
+  longitude: 103.82148508241478,
+  zoom: 10.3,
+  bearing: 0,
+  pitch: 0
+};
+
+const map = new Map({
+  mapboxApiAccessToken: MAPBOX_TOKEN,
+  container: "map",
+  style: "mapbox://styles/mapbox/light-v9",
+  viewState: INITIAL_VIEW_STATE
+});
+
+export const deck = new Deck({
+  canvas: "deck-canvas",
+  width: "100%",
+  height: "100%",
+  initialViewState: INITIAL_VIEW_STATE,
+  controller: MapController,
+  onViewStateChange: ({ viewState }) => {
+    map.setProps({ viewState });
+  },
+  layers: [
+    new GeoJsonLayer({
+      data: GEOJSON,
+      stroked: true,
+      filled: true,
+      lineWidthMinPixels: 2,
+      opacity: 0.4,
+      getLineColor: () => [255, 100, 100],
+      getFillColor: () => [200, 160, 0, 180]
+    })
+  ]
+});
